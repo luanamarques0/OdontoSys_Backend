af396da69ca4081b1bbc4884e5b962dc2e8119af
luanamarques0 <luanamarques.silv@gmail.com>
2025-06-21 23:27:53 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/ifpe/br/odontosys/OdontosysApplication.java
@@ -0,0 +1,13 @@
+package com.ifpe.br.odontosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdontosysApplication.class, args);
+	}
+
+}
