45f1e279ba01a27167117fbc9249ef539549aeab
AlissonLimaG <alissongabriel010907@gmail.com>
2025-07-14 12:31:13 -0300

    Retirando importação desnecessária

--- src/main/java/com/ifpe/br/odontosys/model/ConsultaModel.java
+++ src/main/java/com/ifpe/br/odontosys/model/ConsultaModel.java
@@ -3,7 +3,6 @@ package com.ifpe.br.odontosys.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.*;
 import java.time.LocalDate;
